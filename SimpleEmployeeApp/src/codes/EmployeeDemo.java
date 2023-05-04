26d6fc2988fe913c43e339e38eba5f185162442c
Subrahmanyam956 <64305872+Subrahmanyam956@users.noreply.github.com>
2023-05-05 01:41:09 +0530

    Add files via upload

---
+++ SimpleEmployeeApp/src/codes/EmployeeDemo.java
@@ -0,0 +1,11 @@
+package codes;
+
+
+
+public class EmployeeDemo {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			Employee  employee = new Employee("abc", "def"); 
+		}
+}
